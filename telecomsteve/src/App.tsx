e96caf97e49e1ebc7c1e1cd669d3348e341d888e
Steven Platt <mr.platt@gmail.com>
2026-01-19 11:56:22 -0800

    removed blog link

--- telecomsteve/src/App.tsx
+++ telecomsteve/src/App.tsx
@@ -35,7 +35,6 @@ const navLinks = [
 	{ label: "Resume", to: "/resume" },
 	{ label: "Research", to: "/research" },
 	{ label: "Portfolio", to: "/portfolio" },
-	{ label: "Blog", to: "/blog" },
 ];
 
 type NavLinkItemProps = {
